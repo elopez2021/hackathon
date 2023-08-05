a76375053d7dedb515e08591aff7f416e0e56067
elopez2021 <edgarlopez19082004@gmail.com>
2023-08-05 14:16:23 -0400

    address added

--- restaurant/login/views.py
+++ restaurant/login/views.py
@@ -4,6 +4,10 @@ from django.shortcuts import redirect, render
 from django.contrib import messages
 from django.urls import reverse
 import requests
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+
+from .decorators import unauthenticated_user
 
 
 # Create your views here.
@@ -18,16 +22,36 @@ def login(request):
             data = response.json()
             # Access the data fields
             field1 = data['success']
-            #field2 = data['data']
-            # Process the data as needed
+
             if field1 == False:
                 messages.error(request, data['error'])
             else:
+                if data.get('success', True):
+                # If authentication is successful, check if the user exists in Django database
+                    try:
+                        user = User.objects.get(username=username)
+                    except User.DoesNotExist:
+                        # If the user does not exist, create one using the API response data
+                        user = User(username=username)
+                        user.set_unusable_password()
+                        user.external_api_authenticated = True  # Flag to indicate the user was authenticated through the API
+                        user.save()
+
+                        user = authenticate(request, username=username, password=password)
+            
+                        if user is not None and user.is_authenticated:
+                            # User is authenticated, log them in
+                            login(request, user)
+                            return redirect('home')
+                        else:
+                            # Authentication failed, handle the error as needed
+                            messages.error(request, "Invalid username or password")
                 if data["data"]['isActive'] and data["data"]['isAdmin'] == False and data["data"]["isReception"] == False:
                     return redirect(reverse('home'))
         else:
             messages.error(request, 'Error en el servidor')
     return render(request, 'user/login.html')
 
+@unauthenticated_user
 def home(request):
     return render(request, 'user/dashboard.html')
