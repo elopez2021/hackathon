f580473ce5dfae3dff6ac859d49f49d2997ac700
elopez2021 <edgarlopez19082004@gmail.com>
2023-08-05 13:13:48 -0400

    making request to the login success

--- restaurant/login/views.py
+++ restaurant/login/views.py
@@ -1,8 +1,28 @@
+import json
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from django.contrib import messages
+import requests
+
 
 # Create your views here.
 def login(request):
     if request.method == 'POST':
         username = request.POST.get("username")
         password = request.POST.get("password")
+        url = 'https://hackathon.voiceteamcall.com?token=dGVhbTEyOjkwcjcyMzh6eQ==' 
+        payload = {'username': username, 'password': password} 
+        response = requests.post(url, data=payload)
+        if response.status_code == 200:
+            data = response.json()
+            # Access the data fields
+            field1 = data['success']
+            #field2 = data['data']
+            # Process the data as needed
+            if field1 == False:
+                messages.error(request, data['error'])
+            else:
+                messages.success(request, data['data']["isAdmin"])
+        else:
+            messages.error(request, 'Error en el servidor')
     return render(request, 'user/login.html')
